f8f9e03180c254b3ef97cd36ad154b431f018209
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-02-21 17:13:29 +0100

    Banzai!

--- wordpress/wp-content/themes/mytheme/banzai/src/components/Navbar.js
+++ wordpress/wp-content/themes/mytheme/banzai/src/components/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container,Image } from 'react-bootstrap';
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="transparent" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Brand href="#home"><Image
+            src="/LogoKitsune.jpg
+            " 
+            
+            width="150"  
+            height="150" 
+            className="d-inline-block align-top"
+          /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">Articoli</Nav.Link>
-            <Nav.Link href="#services">Utenti</Nav.Link>
-            <Nav.Link href="#contact"></Nav.Link>
+            <Nav.Link  className="text-dark" href="#home">Home</Nav.Link>
+            <Nav.Link className="text-dark" href="#about">Articoli</Nav.Link>
+            <Nav.Link className="text-dark" href="#services">Utenti</Nav.Link>
+            <Nav.Link className="text-dark" href="#contact"></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
